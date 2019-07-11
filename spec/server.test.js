a1752f71d4f7c1433f4425cc0bd81a228fb5404b
glkapitan <galinaberger19@gmail.com>
2019-07-11 13:19:52 -0400

    added fixes to server.test and added tests for Products and more Info data

--- spec/server.test.js
+++ spec/server.test.js
@@ -1,17 +1,28 @@
+const request = require('supertest');
 
 
 describe('Express server', () => {
 
-  test('should send valid response to get request to /api/reviews', async () => {
-    const response = await request('http://127.0.0.1:3001')
-      .get('/api/reviews')
-      .send({
-        filmname:  "The Shawshank Redemption"
-      });
+
+    test('should send valid response to get request to /api/reviews', async () => {
+     const response = await request('http://127.0.0.1:3001')
+    .get('/api/reviews?filmname=The Shawshank Redemption');
+
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(1);
     expect(response.body[0]).toHaveProperty("reviews");
     expect(response.body[0]).toHaveProperty("filmname");
   });
-})
 
+
+  test('should send valid response to get request to /api/products', async () => {
+    const response = await request('http://127.0.0.1:3001')
+   .get('/api/products');
+
+   expect(response.statusCode).toBe(200);
+   expect(response.body.length).toBe(100);
+   expect(response.body[0]).toHaveProperty("prodname");
+   expect(response.body[0]).toHaveProperty("price");
+ });
+
+});
