b6d0ff7c30bad708b0671906507b89bfbf28de52
amol gajewar <amolgajewar@gmail.com>
2019-07-25 08:13:13 -0700

    added apis to handle create, update and delete requests for reviews collection

--- server/index.js
+++ server/index.js
@@ -14,13 +14,59 @@ app.get('/', (req, res) => {
   res.send(200);
 });
 
+// read api
 app.get('/api/reviews', (req, res) => {
-  //console.log(req.query)
-     db.getReviewsforFilm(req.query.id)
+    db.getReviewsforFilm(req.query.id)
     .then(results => res.send(results))
-    .catch(err => console.log(err))
+    .catch(err => {
+      res.status(500).send();
+      res.end();
+      console.log(err);
+    })
 });
 
+// create api
+app.post('/api/reviews', (req, res) => {
+  db.saveReviewsforFilm(JSON.parse(JSON.stringify(req.body)))
+  .then(results => res.status(201).json(results))
+  .then(()=> res.end())
+  .catch(err => {
+    res.status(500).send();
+    res.end();
+    console.log(err);
+  })
+});
+
+// update api
+app.put('/api/reviews', (req, res) => {
+  const reqObj = JSON.parse(JSON.stringify(req.body));
+  console.log(reqObj);
+  const updateQuery = reqObj.updateQuery;
+  const updateValues = reqObj.updateValues;
+
+  db.updateReviewsforFilm(updateQuery, updateValues)
+  .then(results => res.status(201).json(results))
+  .catch(err => {
+    res.status(500).send();
+    res.end();
+    console.log(err);
+  })
+});
+
+// delete api
+app.delete('/api/reviews', (req, res) => {
+  const reqObj = JSON.parse(JSON.stringify(req.body));
+  console.log(reqObj);
+  const deleteQuery = reqObj.deleteQuery;
+
+  db.deleteAllReviewsforFilm(deleteQuery)
+  .then(results => res.status(200).json(results))
+  .catch(err => {
+    res.status(500).send();
+    res.end();
+    console.log(err);
+  })
+});
 
 
 app.get('/api/products', (req, res) => {
